fce3c10b1606af4edcf0028cb4672fb198b5fccc
costagavras <cgavras@yahoo.ca>
2018-12-16 22:36:17 -0500

    interpolation

--- data-flow-control.rb
+++ data-flow-control.rb
@@ -6,3 +6,23 @@ puts "A very young or old man" if gender == "male" && (age < 18 || age > 85)
 1.upto(5) {|number| puts number}
 10.downto(5) {|number| puts number}
 10.step(20, 5) {|number| puts number}
+
+#Strings
+puts x = %q{This is
+going to be output
+over three lines}
+
+puts x = <<LINE_END
+Do you think
+this will work?
+LINE_END
+
+puts "Success!" if "Test" + "String" == "TestString"
+
+puts "A".ord
+puts "B".ord
+
+x = 5
+y = 1
+puts "#{x} + #{y} = #{x + y}"
+puts x.to_s + " + " + y.to_s + " = " + (x + y).to_s
