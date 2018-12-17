eac0eeca0ea0c72d7e7ea353289d7727125a5980
costagavras <cgavras@yahoo.ca>
2018-12-16 20:46:03 -0500

    iterators

---
+++ data-flow-control.rb
@@ -0,0 +1,8 @@
+gender = "male"
+age = 6
+puts "A very young or old man" if gender == "male" && (age < 18 || age > 85)
+
+
+1.upto(5) {|number| puts number}
+10.downto(5) {|number| puts number}
+10.step(20, 5) {|number| puts number}
