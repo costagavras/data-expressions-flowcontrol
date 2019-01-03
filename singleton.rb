37ace2ef8c9c6b7f50045981cea1eef02f1a7e67
costagavras <cgavras@yahoo.ca>
2019-01-03 17:06:36 -0500

    singleton class

--- singleton.rb
+++ singleton.rb
@@ -1,11 +1,21 @@
-#singleton = method defined on a per-object basis
+#singleton = method defined on a per-object basis as opposed to per-class
 
 str = "Hello, world!"
 # str2 = "Goodbye!"
 
 def str.spell
-  self.split(/./).join("-")
+  self.split("").join("-")
 end
 
 puts str.spell
 # puts str2.spell
+
+str = "hello"
+
+class << str
+  def hyphenated
+    self.split("").join("-")
+  end
+end
+
+puts str.hyphenated
