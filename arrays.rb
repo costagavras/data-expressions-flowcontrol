7ef7a1663dbed7714beff9ed49dcd85604abd4b1
costagavras <cgavras@yahoo.ca>
2019-01-09 17:43:29 -0500

    union, intersection, remove nil

--- arrays.rb
+++ arrays.rb
@@ -12,3 +12,16 @@ puts a.grep(/ary/){|m| m.length}
 b = [1,20,3,4,49,9,24]
 puts b.grep(12..32)
 puts b.grep(12..32) {|n| n*n}
+#index of the min, alphabetical, April
+puts a.index a.min
+
+#Array union and intersection
+a = [1, 2, 3, 4, 5]
+b = [3, 4, 5, 6, 7]
+#duplicates are removed
+p c = a | b
+#only duplicates
+p d = a & b
+
+a = [1, 2, nil, 4, nil, 7]
+b = a.compact #[1,2,4,7]
